84ad547503dc605c35a953698a7bbbeaffdb8878
kljoao <joao.ramos@infocrerj.local>
2024-02-29 17:14:45 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- ICON IMPORT -->
+    <link rel="shortcut icon" href="assets/imgfavicon.ico" />
+
+    <!-- CSS IMPORT -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- FONT AWESOME IMPORT -->
+    <script src="https://kit.fontawesome.com/4713c304f5.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <header class="header">
+        <div>
+            <img src="assets/img/logo-empresas.png" alt="" class="sicoob-logo">
+        </div>
+        <div class="header-itens">
+            <ul class="header-itens-links">
+                <a href="">
+                    <li class="header-item norma-asap-itens-white">Início</li>
+                </a>
+                <a href="">
+                    <li class="header-item norma-asap-itens-white">Ramais</li>
+                </a>
+                <a href="">
+                    <li class="header-item norma-asap-itens-white">Chamados</li>
+                </a>
+                <a href="">
+                    <li class="header-item norma-asap-itens-white">Docs</li>
+                </a>
+                <a href="">
+                    <li class="header-item norma-asap-itens-white">FAQ</li>
+                </a>
+            </ul>
+
+            <span class="header-itens-separador"></span>
+
+            <div class="header-itens">
+                <ul>
+                    <a href="">
+                        <li class="norma-asap-itens-white">Login</li>
+                    </a>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="">
+                            <button class="admin-button norma-asap-itens-black" style="cursor:pointer;">
+                                Administrador
+                            </button>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    </header>
+
+    
+    <hr class="header-separador">
+</body>
+</html>
