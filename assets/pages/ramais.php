cb6315c1a913525d1d11802498ad3c6ec7e151c5
kljoao <joao.ramos@infocrerj.local>
2024-03-04 17:46:25 -0300

    page-form

---
+++ assets/pages/ramais.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oficial Sicoob Empresas - Intranet</title>
+
+    <!-- ICON IMPORT -->
+    <link rel="shortcut icon" href="assets/imgfavicon.ico" />
+
+    <!-- CSS IMPORT -->
+    <link rel="stylesheet" href="../../style.css">
+
+    <!-- FONT AWESOME IMPORT -->
+    <script src="https://kit.fontawesome.com/4713c304f5.js" crossorigin="anonymous"></script>
+
+    <!-- TAILWIND IMPORT -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+</head>
+<body>
+    <header class="header">
+        <div>
+            <img src="../img/logo-empresas.png" alt="" class="sicoob-logo">
+        </div>
+        <div class="header-itens">
+            <ul class="header-itens-links">
+                <a href="">
+                    <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Início</li norma-asap-itens-white-small>
+                </a>
+                <a href="">
+                    <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Ramais</li norma-asap-itens-white-small>
+                </a>
+                <a href="">
+                    <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Chamados</li norma-asap-itens-white-small>
+                </a>
+                <a href="">
+                    <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Links</li norma-asap-itens-white-small>
+                </a>
+                <a href="">
+                    <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">FAQ</li norma-asap-itens-white-small>
+                </a>
+            </ul>
+
+            <span class="header-itens-separador"></span>
+
+            <div class="header-itens">
+                <ul>
+                    <a href="">
+                        <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Login</li norma-asap-itens-white-small>
+                    </a>
+                </ul>
+                <ul>
+                    <li norma-asap-itens-white-small>
+                        <a href="">
+                            <button class="admin-button norma-asap-itens-black" style="cursor:pointer;">
+                                Administrador
+                            </button>
+                        </a>
+                    </li norma-asap-itens-white-small>
+                </ul>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    </header>
+
+    <hr class="header-separador">
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Oficial Sicoob Empresas - Intranet</title>
 
     <!-- ICON IMPORT -->
     <link rel="shortcut icon" href="assets/imgfavicon.ico" />
@@ -14,6 +14,10 @@
     <!-- FONT AWESOME IMPORT -->
     <script src="https://kit.fontawesome.com/4713c304f5.js" crossorigin="anonymous"></script>
 
+    <!-- TAILWIND IMPORT -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
 </head>
 <body>
@@ -23,16 +27,16 @@
         </div>
         <div class="header-itens">
             <ul class="header-itens-links">
-                <a href="">
+                <a href="index.php">
                     <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Início</li norma-asap-itens-white-small>
                 </a>
-                <a href="">
+                <a href="assets/pages/ramais.php">
                     <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Ramais</li norma-asap-itens-white-small>
                 </a>
-                <a href="">
+                <a href="#chamados">
                     <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Chamados</li norma-asap-itens-white-small>
                 </a>
-                <a href="">
+                <a href="#links">
                     <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Links</li norma-asap-itens-white-small>
                 </a>
                 <a href="">
@@ -45,7 +49,7 @@
             <div class="header-itens">
                 <ul>
                     <a href="">
-                        <li norma-asap-itens-white-small class="norma-asap-itens-white-small">Login</li norma-asap-itens-white-small>
+                        <li norma-asap-itens-white-small class="header-item norma-asap-itens-white-small">Login</li norma-asap-itens-white-small>
                     </a>
                 </ul>
                 <ul>
@@ -106,19 +110,31 @@
         </div>
     </section>
 
-    <section class="index-ramal-container">
-        <div class="index-ramal">
-            <div>
-                <h1 class="norma-asap-itens-white-big" style="color: white;">Com Algumas Urgência?<br>Use nossos <span class="ramais">Ramais</span></h1>
-                <p class="norma-asap-itens-white-small" style="color: rgba(255, 255, 255, 0.582); margin-top: 10px;">O ramal só deverá ser usado em último caso (emergências),<br>utilize outras formas de contato como e-mail ou teams.</p>
-                <div class="index-ramais-buttons">
-                    <button class="norma-asap-itens-white-small btnPrincipal" style="color: white !important;">Acessar Ramais</button>
-                    <button class="norma-asap-itens-white-small btnSecond" style="color: white !important;">Relatar Problemas</button>
-                </div>
+    <section class="apresentacao-container">
+        <div class="apresentacao-sistema">
+            <h1 class="norma-asap-itens-white-big" style="color: white;">Um novo sistema integrado e amigável</h1>
+            <p class="norma-asap-itens-white-small" style="color: rgba(255, 255, 255, 0.582);">Intranet que conecta tudo a todos.</p>
+        </div>
+
+        <div class="apresentacao-items">
+
+            <div class="apresentação-i-contorno-left">
+                <h1 class="norma-asap-itens-white-big">100%</h1>
+                <p class="norma-asap-itens-white-small">Novo</p>
             </div>
-            <div>
-                <img src="assets/img/ramal.svg" alt="" width="520px">
+            <div class="apresentação-i">
+                <h1 class="norma-asap-itens-white-big">43%</h1>
+                <p class="norma-asap-itens-white-small">Mais otimizado</p>
             </div>
+            <div class="apresentação-i">
+                <h1 class="norma-asap-itens-white-big">6</h1>
+                <p class="norma-asap-itens-white-small">Novas funcionalidades</p>
+            </div>
+            <div class="apresentação-i-contorno-right">
+                <h1 class="norma-asap-itens-white-big">6</h1>
+                <p class="norma-asap-itens-white-small">Novas funcionalidades</p>
+            </div>
+
         </div>
     </section>
 
@@ -148,7 +164,155 @@
         </div>
     </section>
 
-    <h1 style="margin-bottom: 500px;">.</h1>
+        <section class="index-ramal-container" id="chamados">
+        <div class="index-ramal">
+            <div>
+                <h1 class="norma-asap-itens-white-big" style="color: white;">Com Algumas Urgência?<br>Use nossos <span class="ramais">Ramais</span></h1>
+                <p class="norma-asap-itens-white-small" style="color: rgba(255, 255, 255, 0.582); margin-top: 10px;">O ramal só deverá ser usado em último caso (emergências),<br>utilize outras formas de contato como e-mail ou teams.</p>
+                <div class="index-ramais-buttons">
+                    <a href="https://app.pipefy.com/public/form/qzIn9XqR"><button class="norma-asap-itens-white-small btnPrincipal" style="color: white !important;">Acessar Ramais</button></a>
+                    <button class="norma-asap-itens-white-small btnSecond" style="color: white !important;">Relatar Problemas</button>
+                </div>
+            </div>
+            <div>
+                <img class="ramalimg"src="assets/img/ramal.svg" alt="" width="520px">
+            </div>
+        </div>
+    </section>
+    <section id="links">
+        <div class="links-container">
+            <h1 class="norma-asap-itens-white-big" style="color: white;">Encontre aqui o que procura</h1>
+            <p class="norma-asap-itens-white-small">Procurando aquele link e não consegue encontrar? Aqui você encontra!</p>
+        </div>
+
+        <div class="links-items">
+
+            <div class="big-links max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <i class="found-icons fa-solid fa-building-columns"></i>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Cursos</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Acesso direto ao universisdade Sicoob, onde você acessa todos os seus cursos.</p>
+                <hr>
+                <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline" style="margin-top: 10px;">
+                    Acessar site
+                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    </svg>
+                </a>
+            </div>
+
+            <div class="big-links max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <i class="found-icons fa-solid fa-ticket-simple"></i>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Portal CCS</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Portal para abertura de chamados junto a Central, ou nossa Confederação.</p>
+                <hr>
+                <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline" style="margin-top: 10px;">
+                    Acessar site
+                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    </svg>
+                </a>
+            </div>
+
+            <div class="big-links max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <i class="found-icons fa-solid fa-money-check-dollar"></i>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Sipagnet</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Link de acesso direto para a plataforma Sipagnet.</p>
+                <hr>
+                <a href="https://web.sipag.com.br/sipagnet/emi/site" class="inline-flex font-medium items-center text-blue-600 hover:underline" style="margin-top: 10px;">
+                    Acessar site
+                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    </svg>
+                </a>
+            </div>
+
+            <br>
+
+            <div class="big-links max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <i class="found-icons fa-solid fa-gear"></i>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Pipefy</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Acompanhamento de processos gerais internos da empresa.</p>
+                <hr>
+                <a href="https://app.pipefy.com/organizations/110391" class="inline-flex font-medium items-center text-blue-600 hover:underline" style="margin-top: 10px;">
+                    Acessar site
+                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    </svg>
+                </a>
+            </div>
+
+        </div>
+
+        
+
+    </section>
+
+    <footer class="relative pt-8 pb-6" style="margin-top: 150px; background-color: black;">
+  <div class="container mx-auto px-4">
+    <div class="flex flex-wrap text-left lg:text-left">
+      <div class="w-full lg:w-6/12 px-4">
+        <h4 class="text-3xl norma-asap-itens-white-big" style="color: white;">Sicoob Empresas</h4>
+        <h5 class="norma-asap-itens-white-small">
+          Intranet oficial do Sicoob Empresas.
+        </h5>
+      </div>
+      <div class="w-full lg:w-6/12 px-4">
+        <div class="flex flex-wrap items-top mb-6">
+          <div class="w-full lg:w-4/12 px-4 ml-auto">
+            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Links Importantes</span>
+            <ul class="list-unstyled">
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">Inicio</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Ramais</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Links</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Chamados</a>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full lg:w-4/12 px-4">
+            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+            <ul class="list-unstyled">
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
+              </li>
+              <li>
+                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="my-6 border-blueGray-300">
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+      <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+        <div class="norma-asap-itens-white-small">
+          Copyright © <span id="get-current-year">2024</span> - Por Sicoob Empresas
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
 
     <script src="assets/scripts/index.js"></script>
 </body>
